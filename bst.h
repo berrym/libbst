a0c8136d4a989938a5645b43911fdb6827a0a644
Michael Berry <trismegustis@gmail.com>
2019-04-10 10:59:15 -0400

    Added comments to bst.h

--- bst.h
+++ bst.h
@@ -7,30 +7,35 @@
 #define CastInt (int *)
 #define CastIntPrintf *(int *)
 
+// Enum for comparison functions
 typedef enum result {
     LESS = -1,
     EQUAL = 0,
     GREATER = 1
 } result;
 
+// Node comparison function prototype
 typedef result (*node_comaprator)(const void *, const void *);
 
+// Binary search tree node
 typedef struct bst_node {
     void *data;
     struct bst_node *left;
     struct bst_node *right;
 } bst_node;
 
+// Type agnostic functions
 bst_node *bst_new_node(size_t, void *);
-bst_node *bst_int_insert(bst_node *, size_t, void *);
 bst_node *bst_min_value(bst_node *);
 bst_node *bst_max_value(bst_node *);
+
+// Integer type specific functions
+bst_node *bst_int_insert(bst_node *, size_t, void *);
 bst_node *bst_remove_int_node(bst_node *, void *);
 void bst_delete_int_tree(bst_node *);
 void bst_int_traverse_inorder(bst_node *);
 void bst_int_traverse_postorder(bst_node *);
 void bst_int_traverse_preorder(bst_node *);
-
 result compare_int(const void *, const void *);
 
 #endif
